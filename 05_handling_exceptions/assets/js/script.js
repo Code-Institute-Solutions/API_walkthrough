abbbb067c2e3c042ce5b699facd7bc7440261605
Matt Rudge <matt.rudge@codeinstitute.net>
2021-02-22 06:57:46 +0000

    add parts 4 & 5

--- 05_handling_exceptions/assets/js/script.js
+++ 05_handling_exceptions/assets/js/script.js
@@ -38,6 +38,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -54,9 +55,23 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayErrors(data) {
